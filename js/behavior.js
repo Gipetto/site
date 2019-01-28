ea6ddeec96394b5a2a8bca2400cf86c1ff2d0c05
Shawn Parker <shawn@topfroggraphics.com>
2019-01-27 23:14:29 -0800

    Hamburgered

--- js/behavior.js
+++ js/behavior.js
@@ -1,2 +1,12 @@
 "use strict";
-// if I do things right this should remain emtpy
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    let nav = document.getElementsByTagName('header')[0]
+        .getElementsByTagName('nav')[0];
+    let hamburger = nav.getElementsByClassName('hamburger')[0];
+    
+    hamburger.getElementsByTagName('a')[0].onclick = function(e) {
+        e.preventDefault();
+        nav.classList.toggle('hamburgled');
+    };
+});
