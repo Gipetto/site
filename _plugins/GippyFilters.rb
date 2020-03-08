575a05106c46245e13cec0ed2fb9fc4e58373a16
Shawn Parker <shawn@topfroggraphics.com>
2020-03-07 20:43:44 -0800

    Reorganize plugins

--- _plugins/GippyFilters.rb
+++ _plugins/GippyFilters.rb
@@ -1,10 +1,13 @@
+require 'digest'
+
 module Jekyll
-    module Tokenize
+    module GippyFilters
         include Jekyll::Filters
 
         @@stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
             .split(",")
 
+        # Reduce a post down to unique tokens for search
         def tokenize(input)
             tokens = Set.new()
 
@@ -19,7 +22,24 @@ module Jekyll
 
             tokens.to_a.join(" ")
         end
+
+        # Prepend paragraph glyph
+        def pp(input)
+            "<i class=\"pp\" aria-hidden=\"true\">§ </i>#{input}"
+        end
+
+        # sha256 a string
+        def sha256(input)
+            Digest::SHA256.base64digest input
+        end
+
+        # Word Case A String
+        def wordcase(input)
+            input.gsub(/\w+/) do |token|
+                token.capitalize
+            end            
+        end
     end
 end
 
-Liquid::Template.register_filter(Jekyll::Tokenize)
+Liquid::Template.register_filter(Jekyll::GippyFilters)
--- _plugins/PPFilter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-module PPFilter
-    def pp(input)
-    "<i class=\"pp\" aria-hidden=\"true\">§</i>#{input}"
-    end
-end
-end
-
-Liquid::Template.register_filter(Jekyll::PPFilter)
